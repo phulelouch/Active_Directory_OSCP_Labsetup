f73a1a60c984547ec5ce92c40bd15eeb8fde3df6
phulelouch <62769629+phulelouch@users.noreply.github.com>
2024-10-29 12:16:07 +1100

    Update TurnOffAV.ps1

--- MS01/TurnOffAV.ps1
+++ MS01/TurnOffAV.ps1
@@ -1,22 +1,5 @@
 $global:version = "1.0.0"
 
-$ascii = @"
-
-.____                        .__            .____          ___.     _________       __                
-|    |    ____   ____ _____  |  |           |    |   _____ \_ |__  /   _____/ _____/  |_ __ ________  
-|    |   /  _ \_/ ___\\__  \ |  |    ______ |    |   \__  \ | __ \ \_____  \_/ __ \   __\  |  \____ \ 
-|    |__(  <_> )  \___ / __ \|  |__ /_____/ |    |___ / __ \| \_\ \/        \  ___/|  | |  |  /  |_> >
-|_______ \____/ \___  >____  /____/         |_______ (____  /___  /_______  /\___  >__| |____/|   __/ 
-        \/          \/     \/                       \/    \/    \/        \/     \/           |__|    
-
-~ Created with <3 by @nickvourd
-~ Version: $global:version
-~ Type: Turn Off AV
-
-"@
-
-Write-Host $ascii`n
-
 Write-Host "[+] Disabling Windows Defender`n"
 
 Set-MpPreference -DisableRealtimeMonitoring $true
